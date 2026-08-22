e1a9a62f392a989125d2666da28c291735848342
redidk <neevs2010@gmail.com>
2026-08-22 16:18:43 -0500

    Update HelloFTC.java
    created first ever java program for ftc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloFTC.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-public class HelloFTC {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//use @Disabled so that this program does not show up on the DS
+@TeleOp
+public class HelloFTC extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello","World");
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
